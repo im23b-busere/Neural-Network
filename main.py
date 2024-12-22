51bcfbd8b5a0c36832730bbbf6ef98e487bdde9e
im23b-busere <erik.buser@student.ksh.ch>
2024-12-23 00:08:33 +0100

    Add backpropagation & print err

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import numpy as np
-from numpy.ma.core import outer
-
 from dataset import inputs, targets
+
 
 # sigmoid function for converting the output in the range of 0 to 1
 def sigmoid(x):
@@ -12,9 +11,11 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return x * (1 - x)
 
+
 # loss function for calculating the error
 def loss_function(real, prediction):
     return np.mean((real - prediction) ** 2)
+
 
 # parameters
 input_layer_neurons = 2
@@ -24,16 +25,15 @@ output_layer_neurons = 1
 # initializing random weights and biases
 np.random.seed(0)
 
-hidden_weights = np.random.uniform(size=(input_layer_neurons, hidden_layer_neurons)) # size to make a matrix of 2x2
-output_weights = np.random.uniform(size=(hidden_layer_neurons, output_layer_neurons)) # size to make a matrix of 2x1
+hidden_weights = np.random.uniform(size=(input_layer_neurons, hidden_layer_neurons))  # size to make a matrix of 2x2
+output_weights = np.random.uniform(size=(hidden_layer_neurons, output_layer_neurons))  # size to make a matrix of 2x1
 
-
-hidden_biases = np.random.uniform(size=(1, hidden_layer_neurons)) # size to make a matrix of 1x2
-output_biases = np.random.uniform(size=(1, output_layer_neurons)) # size to make a matrix of 1x1
+hidden_biases = np.random.uniform(size=(1, hidden_layer_neurons))  # size to make a matrix of 1x2
+output_biases = np.random.uniform(size=(1, output_layer_neurons))  # size to make a matrix of 1x1
 
 # learning rate & cycles
-lr = 0.1 # tells at what rate the weights and biases should be updated
-cycles = 10000 # number of times the model should be trained
+lr = 0.1  # tells at what rate the weights and biases should be updated
+cycles = 10000  # number of times the model should be trained
 
 # main training loop
 # multiplies each neuron with the weights and then adds the biases
@@ -48,10 +48,21 @@ output = sigmoid(output_layer_input)
 # calculate the error
 error = output - targets
 
-print("Hidden Layer Input: ", hidden_layer_input)
-print("Hidden Layer Output: ", hidden_layer_output)
-print("Output Layer Input: ", output_layer_input)
-print("Output: ", output)
-print("Error: ", error)
+# backpropagation
+# calculate the derivative of the output layer
+derivative_output = error * sigmoid_derivative(output)
 
+# calculate the derivative of the hidden layer
+error_hidden_layer = derivative_output.dot(output_weights.T)
+d_hidden_layer = error_hidden_layer * sigmoid_derivative(hidden_layer_output)
 
+# update the weights and biases
+output_weights -= hidden_layer_output.T.dot(derivative_output) * lr
+hidden_weights -= inputs.T.dot(d_hidden_layer) * lr
+
+output_biases -= np.sum(derivative_output) * lr
+hidden_biases -= np.sum(d_hidden_layer) * lr
+
+# print the error
+loss = loss_function(targets, output)
+print(f"Error: {loss}")
