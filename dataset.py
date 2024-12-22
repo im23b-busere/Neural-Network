c5d7cf4f3919534e1da85e534628735c7977ece9
im23b-busere <erik.buser@student.ksh.ch>
2024-12-22 01:44:59 +0100

    initializing random weights & biases + test

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
 
+# inputs with XOR values (1+0=1, 1+1=0, 0+0=0, 0+1=1)
 inputs = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 targets = np.array([[0], [1], [1], [0]])
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+from numpy.ma.core import outer
+
 from dataset import inputs, targets
 
 # sigmoid function for putting the output in the range of 0 to 1
@@ -13,3 +15,21 @@ def sigmoid_derivative(x):
 # loss function for calculating the error
 def loss_function(real, prediction):
     return np.mean((real - prediction) ** 2)
+
+# parameters
+input_layer_neurons = 2
+hidden_layer_neurons = 2
+output_layer_neurons = 1
+
+# initializing random weights and biases
+np.random.seed(0)
+
+hidden_weights = np.random.uniform(size=(input_layer_neurons, hidden_layer_neurons)) # size to make a matrix of 2x2
+print(hidden_weights)
+output_weights = np.random.uniform(size=(hidden_layer_neurons, output_layer_neurons)) # size to make a matrix of 2x1
+print(output_weights)
+
+hidden_biases = np.random.uniform(size=(1, hidden_layer_neurons)) # size to make a matrix of 1x2
+print(hidden_biases)
+output_biases = np.random.uniform(size=(1, output_layer_neurons)) # size to make a matrix of 1x1
+print(output_biases)
